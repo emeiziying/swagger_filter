a6a0301146bc5985fc25d5d8a342d7bf8f30e522
GitHub Action <action@github.com>
2025-07-12 18:02:42 +0000

    chore: bump version to 1.0.2

--- lib/version.dart
+++ lib/version.dart
@@ -1,5 +1,5 @@
 /// Version information for swagger_filter package
-const String packageVersion = '1.0.1';
+const String packageVersion = '1.0.2';
 const String packageName = 'swagger_filter';
 
 /// Display version information
